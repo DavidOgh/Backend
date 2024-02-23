85892821f003f72a30368e330cbfaffa092cada1
Lucian <duta39@gmail.com>
2024-02-23 12:30:35 +0000

    fix: :bug: fixed missing validator function

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -3,6 +3,7 @@ import {
   generateDublinWeatherData,
   generateLondonWeatherData,
 } from "../services/weatherService.js";
+import { validationResult } from "express-validator";
 
 /**
  * Gets the weather data for a city
@@ -10,6 +11,16 @@ import {
  * @param res the response object
  */
 export const getWeatherData = async (req: Request, res: Response) => {
+  // We will use the validationResult function to check if there are any validation errors
+  const errors = validationResult(req);
+
+  // If there are validation errors, we will log them and send a 400 status code
+  if (!errors.isEmpty()) {
+    console.error("Validation error", errors.mapped());
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+
   // We will use a try catch block to catch any errors
   try {
     // Get the city param from the request
