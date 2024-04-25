9c089cadecf87165ab6f22253b4848c136f2efc5
DavidOgh <160128626+DavidOgh@users.noreply.github.com>
2024-04-25 14:50:13 +0000

    added the new responses to weather route

--- src/routes/weatherRoutes.ts
+++ src/routes/weatherRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { getWeatherData } from '../controllers/weatherController.js';
+import { getWeatherData, getWeatherDayData, getstormPath } from '../controllers/weatherController.js';
 import { validateCityName } from '../middleware/validators.js';
 
 // We will create a router object
 const router = express.Router();
 
 // We will create a route for the weather data based on the city name
-router.get('/:city', validateCityName, getWeatherData);
+router.get('/:city', validateCityName, getWeatherData, getWeatherDayData, getstormPath);
 
 // We will export the router
 export default router;
