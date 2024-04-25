325f4440dedfda35ae0649f872ec3f0027baf1f1
DavidOgh <160128626+DavidOgh@users.noreply.github.com>
2024-04-25 15:50:22 +0000

    creation of stormRouts file in routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import weatherRoute from './routes/weatherRoutes.js';
+import stormRoute from './routes/stormRoutes.js';
 import cors from "cors";
 
 // We will create an express app
@@ -13,7 +14,7 @@ app.use(express.json());
 
 // We define our first route
 app.use('/api/weather', weatherRoute);
-
+app.use('/api/storm', stormRoute);
 // Start the express server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
