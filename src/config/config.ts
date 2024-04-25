34746566cf6f8f6063f7f423eabf740e6fce81bc
DavidOgh <160128626+DavidOgh@users.noreply.github.com>
2024-04-25 18:48:24 +0000

    replaced monomatic

---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+import algosdk from "algosdk";
+
+const algodToken = "a".repeat(64);
+const server: string = "http://localhost";
+const port: string = "4001";
+
+const mnemonic: string =  "your mnemonic";
+
+export function getClient(): algosdk.Algodv2 {
+    let client = new algosdk.Algodv2(algodToken, server, port);
+    return client;
+}
+
+export function getAccount(): algosdk.Account {
+    let account = algosdk.mnemonicToSecretKey("special armor duck uphold hollow reflect supreme high swallow crawl awkward man risk theme keep shrug luggage cart either announce trend remain across abandon sock");
+    return account; 
+}
