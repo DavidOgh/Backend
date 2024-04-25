15b9ef4e64292810c02db2a19318258a555d8325
DavidOgh <160128626+DavidOgh@users.noreply.github.com>
2024-04-25 14:06:58 +0000

    added city to the API responses

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker';
 export const generateLondonWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData = {
+    city: "London",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
@@ -16,6 +17,7 @@ export const generateLondonWeatherData = (): WeatherData => {
 export const generateDublinWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData: WeatherData = {
+    city: "Dublin",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
